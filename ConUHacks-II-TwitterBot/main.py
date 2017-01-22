c4b632184ded51e107153d2f62e1330878923500
Georgantas <john.georan@icloud.com>
2017-01-22 11:08:05 -0500

    The bot. Updated README.

---
+++ ConUHacks-II-TwitterBot/main.py
@@ -0,0 +1,104 @@
+import tweepy
+from monkeylearn import MonkeyLearn
+import time
+import requests
+
+# Monkey Learn Credentials
+ml = MonkeyLearn('47dde3e5ab73adb5379ca8b71d1f47d03befa89f')
+module_id = 'ex_y7BPYzNG'
+
+# Tweepy Credentials
+auth = tweepy.OAuthHandler("Z0FJOWBHhYDv3Hn5cYyJ22EnY", "JnpbFUKF7IRJcCgm7C7GHJN6QTOtL2wY7crNnGtOb0xRJi3Eat")
+auth.set_access_token("822933243880243200-LTlKtstwQ5M4Vghd3NdAWd7VZDFKZMU", "iS5TZMZ2TzLnHxrwB7vF2tQAcUJ6jIq8yR9S7vlSDTh4c")
+
+JSONrequest = "http://api.sandbox.yellowapi.com/FindBusiness/?what=%s&where=Montreal&pgLen=1&pg=1&dist=1&fmt=JSON&lang=en&UID=172.31.109.198&apikey=tst3bbg2kzkdgnscystpbk6j"
+
+# File containing tweets already replied to
+already_replied_file = open('already_replied.txt', 'a+')
+
+# Create Tweepy Object
+api = tweepy.API(auth)
+
+def find_business(sentence):
+  sentence = requests.get(JSONrequest % sentence)
+  sentence = sentence.json()
+  try:
+    sentence = "http://www.yellowpages.ca/bus/Quebec/Montreal/Yellow-Pages/%s.html Checkout %s at %s %s, %s %s!" % (sentence['listings'][0]['id'], sentence['listings'][0]['name'], sentence['listings'][0]['address']['street'], sentence['listings'][0]['address']['city'], sentence['listings'][0]['address']['prov'], sentence['listings'][0]['address']['pcode'])
+  except:
+    sentence = "Couldn't find anything on Yellow Pages!"
+  return sentence
+
+def have_replied(tweet_id):
+  already_replied_file.seek(0)
+  replied_tweet_ids = already_replied_file.readlines()
+  return replied_tweet_ids.count("%s\n" % tweet_id) != 0
+
+def find_keyword(sentence):
+  sentence = [sentence]
+  try:
+    res = ml.extractors.extract(module_id, sentence)
+    res = res.result[0][0]['keyword']
+  except:
+    res = "jrioeht43hr4u3chru34ty8349"
+  return res
+
+def cut_to_140_chars(sentence):
+  if len(sentence) > 140:
+    sentence = sentence[0:139] + '…'
+  return sentence
+
+def reference_author(author, sentence):
+  sentence = '@%s %s' % (author, sentence)
+  return sentence
+
+def is_a_reply(sentence):
+  return sentence.find('@') != -1
+
+def remove_tags(sentence):
+  sentence = sentence.split(' ')
+  indexes_to_delete = []
+  for word in sentence:
+    # Detect indexes to delete
+    if word[0] == '#':
+      indexes_to_delete.append(sentence.index(word))
+  count = 0
+  for index in indexes_to_delete:
+    del sentence[index - count]
+    count = count + 1
+  sentence = ' '.join(sentence)
+  return sentence
+
+def check_tweets():
+  # Search tweets with the keyword
+  public_tweets = api.search("#askYP", rpp=100, since_id=822903540108722178)
+
+  for tweet in public_tweets:
+    # Skip if this is a reply tweet
+    if is_a_reply(tweet.text) or have_replied(tweet.id):
+      continue
+
+    reply_text = tweet.text
+    reply_text = remove_tags(reply_text)
+    reply_text = find_keyword(reply_text)
+    reply_text = find_business(reply_text)
+    reply_text = reference_author(tweet.author.screen_name, reply_text)
+    reply_text = cut_to_140_chars(reply_text)
+
+    print(reply_text)
+
+    try:
+      api.update_status(reply_text, tweet.id)
+      # Add tweet id to list of replied tweets
+      already_replied_file.write("%s\n" % tweet.id)
+      already_replied_file.flush()
+    except:
+      print("FAILED TO REPLY TO TWEET!")
+
+    time.sleep(3)
+
+
+while(True):
+  check_tweets()
+  time.sleep(30)
+
+
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# YP-T2
-YP-2T is a twitter bot that replies to tweets with the #askYP tag. It identifies keywords and returns a YP search.
+## Inspiration
+The YellowPages API Challenge!
+
+## What it does
+YP-2T searches tweets containing the #askYP hashtag. It parses the tweet and inputs it to a machine learning platform called MonkeyLearn. This platform detects keywords in the tweet. The keywords are then passed to the YellowPages API, which returns a business in Montreal related to these words. YP-2T then marks that the tweet was already responded to in a text file. This ensures that it doesn't provide an answer to a tweet twice, as well as ensuring that we don't surpass the Twitter API limits.
+
+## How we built it
+We wrote a python script that runs on a server that periodically checks Twitter search for updates using the Twitter REST API. This script uses the MonkeyLearn machine learning platform to extract keywords from the tweet. These keywords are inputted into the YellowPages API, and the result is returned via a Twitter reply on the initial tweet.
+
+## Challenges we ran into
+* Learning the different APIs. We used a total of three for this project (YellowPages, MonkeyLearn and Twitter REST). We ran into many bugs before getting this platforms working.
+* The 140 character tweet limit. Ensuring not to pass to pass this limit and parsing accordingly.
+* Our twitter account got blocked twice because we were sending too many requests.
+* One of the team members fell sick with the flu.
+
+## Accomplishments that we're proud of
+This is our second hackathon. Last time, we didn't submit a project, so we're very proud of our submission this time around. We're very confident in our project.
+
+## What we learned
+* How to write better Python code (we only knew the very basic fundamentals prior to the hackathon).
+* How to use three different APIs. We knew nothing about these before the hackathon.
+* Working as a team and splitting tasks.
+* Better appreciation for Git, the version control software. We used this to manage our project.
+
+
+## What's next for YP-2T
+YP-2T is complete. The software can be further improved by making it more stable and testing corner cases. Furthermore, we would also like to refactor the code to make it more clean and readable.
